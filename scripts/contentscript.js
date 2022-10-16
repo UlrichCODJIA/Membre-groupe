90b3b44e6f23b2852043cf3b243822c39302300e
Armel CODJIA <codjiaulrich5@outlook.fr>
2022-10-16 11:12:03 +0200

    moved download process to contentscript

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -1,4 +1,25 @@
 const group_member_count = parseInt(/group_member_profiles":{"count":([^"]+),/g.exec(document.body.innerHTML)[1]);
+var numbers = Array.from(range(8000, 1008000, 8000));
+
+let table = document.createElement('table');
+table.style.display = none;
+let thead = document.createElement('thead');
+var name_elmt = document.createElement("th");
+var name_text = document.createTextNode("Firstname / Lastname");
+name_elmt.appendChild(name_text);
+var uid_elmt = document.createElement("th");
+var uid_elmt_text = document.createTextNode("UID");
+uid_elmt.appendChild(uid_elmt_text);
+var tr = document.createElement("tr");
+tr.appendChild(name_elmt);
+tr.appendChild(uid_elmt);
+thead.appendChild(tr);
+let tbody = document.createElement('tbody');
+table.appendChild(thead);
+table.appendChild(tbody);
+
+// Adding the entire table to the body tag
+document.getElementsByTagName('body')[0].appendChild(table);
 
 chrome.runtime.onMessage.addListener((response, callback) => {
     if (response.message == "start_membre_groupe") {
@@ -6,14 +27,90 @@ chrome.runtime.onMessage.addListener((response, callback) => {
             "%cStart Membre-Groupe",
             "color: white; font-weight: 900; font-size: 25px; background-color: blue;padding: 2px"
         );
-        if (group_member_count < 12500) {
+        if (group_member_count < 8500) {
             console.log("%cYou need to wait approximatively 10min or less for the extraction to finish.", "font-weight: 900; font-size: 20px")
         } else {
-            console.log(`%cYou need to wait approximatively ${(Math.round(group_member_count / 10000) * 10) + ((Math.round(group_member_count / 10000) - 1) * 30)}min or less for the extraction to finish.`, "font-weight: 900; font-size: 20px");
+            console.log(`%cYou need to wait approximatively ${(Math.round(group_member_count / 8000) * 10) + ((Math.round(group_member_count / 8000) - 1) * 30)}min or less for the extraction to finish.`, "font-weight: 900; font-size: 20px");
         };
         get_first_uid_and_cursor();
     }
 });
+
+function onStartedDownload(id) {
+    console.log(`Started downloading: ${id}`);
+}
+
+function onFailed(error) {
+    console.log(`Download failed: ${error}`);
+}
+
+function download(profiles_hrefs, profiles_href_length) {
+    var time = setInterval(() => {
+        if (profiles_href_length == Object.keys(profiles_hrefs).length) {
+            clearInterval(time);
+            var allEntries = "";
+            for (const i in profiles_hrefs) {
+                allEntries = allEntries.concat(i + " : " + profiles_hrefs[i] + "\n");
+                var no = document.createElement("td");
+                var no_text = document.createTextNode(i);
+                no.appendChild(no_text);
+                var uid = document.createElement("td");
+                var uid_text = document.createTextNode(profiles_hrefs[i]);
+                uid.appendChild(uid_text);
+                var tr = document.createElement("tr");
+                tr.appendChild(no);
+                tr.appendChild(uid);
+                tbody.appendChild(tr);
+            }
+            const now = new Date();
+            const blob = new Blob([allEntries], {
+                type: "text/plain",
+            });
+            var url = URL.createObjectURL(blob);
+            chrome.downloads
+                .download({
+                    url: url,
+                    filename:
+                        "Membre-Groupe/" +
+                        "Report-" +
+                        now.getFullYear() +
+                        "-" +
+                        now.getMonth() +
+                        "-" +
+                        now.getDate() +
+                        " at " +
+                        now.getHours() +
+                        "_" +
+                        now.getMinutes() +
+                        "_" +
+                        now.getMilliseconds() +
+                        ".txt",
+                    conflictAction: "uniquify",
+                })
+                .then(onStartedDownload, onFailed);
+            /* Create worksheet from HTML DOM TABLE */
+            var wb = XLSX.utils.table_to_book(table);
+            /* Export to file (start a download) */
+            XLSX.writeFile(
+                wb,
+                "Membre-Groupe/" +
+                "Report-" +
+                now.getFullYear() +
+                "-" +
+                now.getMonth() +
+                "-" +
+                now.getDate() +
+                " at " +
+                now.getHours() +
+                "_" +
+                now.getMinutes() +
+                "_" +
+                now.getMilliseconds() +
+                ".xlsx"
+            );
+        }
+    });
+}
 
 function* range(start = 0, end = null, step = 1) {
     if (end == null) {
@@ -25,8 +122,6 @@ function* range(start = 0, end = null, step = 1) {
         yield i;
     }
 }
-
-var numbers = Array.from(range(10000, 1010000, 10000));
 
 async function user_get_request_waiter(firsts_users_uid, cursor, group_id_and_id, doc_id, lsd, __spin_t, __spin_b, __rev_and__spin_r, fb_dtsg, __hsi, jazoest, __comet_req, __user, __a) {
     console.clear();
@@ -42,44 +137,33 @@ async function user_get_request_waiter(firsts_users_uid, cursor, group_id_and_id
                 , 1800000)
         );
 
-    if (Object.keys(firsts_users_uid).length < numbers.slice(-1)[0] && numbers[0] <= group_member_count + 41000) {
+    if (Object.keys(firsts_users_uid).length < numbers.slice(-1)[0] && numbers[0] <= group_member_count + 32800) {
         const uppercaseItem = await asyncUppercase('a');
         console.clear();
-        get_uid(progressCallback, firsts_users_uid, cursor, group_id_and_id, doc_id, lsd, __spin_t, __spin_b, __rev_and__spin_r, fb_dtsg, __hsi, jazoest, __comet_req, __user, __a, Object.keys(firsts_users_uid).length + 10000)
+        get_uid(progressCallback, firsts_users_uid, cursor, group_id_and_id, doc_id, lsd, __spin_t, __spin_b, __rev_and__spin_r, fb_dtsg, __hsi, jazoest, __comet_req, __user, __a, Object.keys(firsts_users_uid).length + 8000)
             .then(result => {
                 if (result.cursor != undefined) {
                     var cursor_res = result.cursor;
                     var firsts_users_uid_res = result.first_user_list;
                     numbers.shift();
-                    user_get_request_waiter(firsts_users_uid_res, cursor_res, group_id_and_id, doc_id, lsd, __spin_t, __spin_b, __rev_and__spin_r, fb_dtsg, __hsi, jazoest, __comet_req, __user, __a, Object.keys(firsts_users_uid_res).length + 10000);
+                    user_get_request_waiter(firsts_users_uid_res, cursor_res, group_id_and_id, doc_id, lsd, __spin_t, __spin_b, __rev_and__spin_r, fb_dtsg, __hsi, jazoest, __comet_req, __user, __a, Object.keys(firsts_users_uid_res).length + 8000);
                 } else if (result.error != undefined) {
-                    chrome.runtime.sendMessage(
-                        { message: "partial_error", error_msg: result.error, first_user_list: result.first_user_list },
-                        function (response) {
-                            console.log(
-                                `%c${result.error.toUpperCase()}. RETRY WHEN THIS FEATURE WILL BE UNLOCKED`,
-                                "color: red; font-weight: 900; font-size: 25px;padding: 2px"
-                            );
-                        }
+                    console.log(
+                        `%c${result.error.toUpperCase()}. RETRY WHEN THIS FEATURE WILL BE UNLOCKED`,
+                        "color: red; font-weight: 900; font-size: 25px;padding: 2px"
                     );
+                    download(result.first_user_list, Object.keys(result.first_user_list).length);
                 } else {
-                    chrome.runtime.sendMessage(
-                        { message: "first_users_uid_and_cursor", first_user_list: result },
-                        function (response) { }
-                    );
+                    download(result, Object.keys(result).length);
                 }
             })
             .catch(console.error);
     } else {
-        chrome.runtime.sendMessage(
-            { message: "error", error_msg: "ERROR TRYING TO GET THE DATAS" },
-            function (response) {
-                console.log(
-                    "%cERROR!!! PLEASE RETRY OR CONTACT ME",
-                    "color: red; font-weight: 900; font-size: 25px;padding: 2px"
-                );
-            }
+        console.log(
+            "%cERROR!!! PLEASE RETRY OR CONTACT ME",
+            "color: red; font-weight: 900; font-size: 25px;padding: 2px"
         );
+        download(firsts_users_uid, Object.keys(firsts_users_uid).length);
     };
 }
 
@@ -138,40 +222,29 @@ async function get_first_uid_and_cursor() {
             "%cExtracting",
             "color: white; font-weight: 900; font-size: 25px; background-color: blue;padding: 2px"
         );
-        get_uid(progressCallback, firsts_users_uid, cursor[1], group_id_and_id, doc_id, lsd, __spin_t, __spin_b, __rev_and__spin_r, fb_dtsg, __hsi, jazoest, __comet_req, __user, __a, 10000)
+        get_uid(progressCallback, firsts_users_uid, cursor[1], group_id_and_id, doc_id, lsd, __spin_t, __spin_b, __rev_and__spin_r, fb_dtsg, __hsi, jazoest, __comet_req, __user, __a, 8000)
             .then(result => {
                 if (result.cursor != undefined) {
                     cursor = result.cursor;
                     firsts_users_uid = result.first_user_list;
                     user_get_request_waiter(firsts_users_uid, cursor, group_id_and_id, doc_id, lsd, __spin_t, __spin_b, __rev_and__spin_r, fb_dtsg, __hsi, jazoest, __comet_req, __user, __a);
                 } else if (result.error != undefined) {
-                    chrome.runtime.sendMessage(
-                        { message: "partial_error", error_msg: result.error, first_user_list: result.first_user_list },
-                        function (response) {
-                            console.log(
-                                `%c${result.error.toUpperCase()}. RETRY WHEN THIS FEATURE WILL BE UNLOCKED`,
-                                "color: red; font-weight: 900; font-size: 25px;padding: 2px"
-                            );
-                        }
+                    console.log(
+                        `%c${result.error.toUpperCase()}. RETRY WHEN THIS FEATURE WILL BE UNLOCKED`,
+                        "color: red; font-weight: 900; font-size: 25px;padding: 2px"
                     );
+                    download(result.first_user_list, Object.keys(result.first_user_list).length);
                 } else {
-                    chrome.runtime.sendMessage(
-                        { message: "first_users_uid_and_cursor", first_user_list: result },
-                        function (response) { }
-                    );
+                    download(result, Object.keys(result).length);
                 }
             })
             .catch(console.error);
     } else {
-        chrome.runtime.sendMessage(
-            { message: "error", error_msg: "ERROR TRYING TO GET THE DATAS" },
-            function (response) {
-                console.log(
-                    "%cERROR!!! PLEASE RETRY OR CONTACT ME",
-                    "color: red; font-weight: 900; font-size: 25px;padding: 2px"
-                );
-            }
+        console.log(
+            "%cERROR!!! PLEASE RETRY OR CONTACT ME",
+            "color: red; font-weight: 900; font-size: 25px;padding: 2px"
         );
+        download(firsts_users_uid, Object.keys(firsts_users_uid).length);
     };
 }
 
@@ -219,7 +292,7 @@ function get_uid(progress, first_user_list, cursor, group_id_and_id, doc_id, lsd
                     for (var i = 0; i < user_list.length; i++) {
                         first_user_list[user_list[i].node.name] = user_list[i].node.id;
                     };
-                } catch(err) {};
+                } catch (err) { };
                 if (new_forum_members != undefined && new_forum_members.page_info.has_next_page == true && Object.keys(first_user_list).length < length_check) {
                     cursor = new_forum_members.page_info.end_cursor
                     progress && progress(first_user_list);
--- scripts/popup.js
+++ scripts/popup.js
@@ -13,105 +13,105 @@ function start() {
 
 get_uid_btn.addEventListener("click", start);
 
-chrome.runtime.onMessage.addListener((response, callback) => {
-    switch (response.message) {
-        case "first_users_uid_and_cursor":
-            download(response.first_user_list, Object.keys(response.first_user_list).length);
-            break;
-        case "error":
-            var h5 = document.createElement("h5");
-            h5.appendChild(document.createTextNode(`Error: ${response.error_msg}`));
-            document.body.children[0].insertBefore(
-                h5,
-                document.body.children[0].children[5]
-            );
-            break;
-        case "partial_error":
-            var h5 = document.createElement("h5");
-            h5.appendChild(document.createTextNode(`Error: ${response.error_msg}`));
-            document.body.children[0].insertBefore(
-                h5,
-                document.body.children[0].children[5]
-            );
-            download(response.first_user_list, Object.keys(response.first_user_list).length);
-            break;
-    }
-});
+// chrome.runtime.onMessage.addListener((response, callback) => {
+//     switch (response.message) {
+//         case "first_users_uid_and_cursor":
+//             download(response.first_user_list, Object.keys(response.first_user_list).length);
+//             break;
+//         case "error":
+//             var h5 = document.createElement("h5");
+//             h5.appendChild(document.createTextNode(`Error: ${response.error_msg}`));
+//             document.body.children[0].insertBefore(
+//                 h5,
+//                 document.body.children[0].children[5]
+//             );
+//             break;
+//         case "partial_error":
+//             var h5 = document.createElement("h5");
+//             h5.appendChild(document.createTextNode(`Error: ${response.error_msg}`));
+//             document.body.children[0].insertBefore(
+//                 h5,
+//                 document.body.children[0].children[5]
+//             );
+//             download(response.first_user_list, Object.keys(response.first_user_list).length);
+//             break;
+//     }
+// });
 
-function onStartedDownload(id) {
-    console.log(`Started downloading: ${id}`);
-}
+// function onStartedDownload(id) {
+//     console.log(`Started downloading: ${id}`);
+// }
 
-function onFailed(error) {
-    console.log(`Download failed: ${error}`);
-}
+// function onFailed(error) {
+//     console.log(`Download failed: ${error}`);
+// }
 
-function download(profiles_hrefs, profiles_href_length) {
-    var time = setInterval(() => {
-        if (profiles_href_length == Object.keys(profiles_hrefs).length) {
-            clearInterval(time);
-            var allEntries = "";
-            for (const i in profiles_hrefs) {
-                allEntries = allEntries.concat(i + " : " + profiles_hrefs[i] + "\n");
-                var no = document.createElement("td");
-                var no_text = document.createTextNode(i);
-                no.appendChild(no_text);
-                var uid = document.createElement("td");
-                var uid_text = document.createTextNode(profiles_hrefs[i]);
-                uid.appendChild(uid_text);
-                var tr = document.createElement("tr");
-                tr.appendChild(no);
-                tr.appendChild(uid);
-                var element = document.getElementById("list_of_uid");
-                element.appendChild(tr);
-            }
-            const now = new Date();
-            loader.style.display = "none";
-            const blob = new Blob([allEntries], {
-                type: "text/plain",
-            });
-            var url = URL.createObjectURL(blob);
-            chrome.downloads
-                .download({
-                    url: url,
-                    filename:
-                        "Membre-Groupe/" +
-                        "Report-" +
-                        now.getFullYear() +
-                        "-" +
-                        now.getMonth() +
-                        "-" +
-                        now.getDate() +
-                        " at " +
-                        now.getHours() +
-                        "_" +
-                        now.getMinutes() +
-                        "_" +
-                        now.getMilliseconds() +
-                        ".txt",
-                    conflictAction: "uniquify",
-                })
-                .then(onStartedDownload, onFailed);
-            /* Create worksheet from HTML DOM TABLE */
-            var wb = XLSX.utils.table_to_book(document.getElementById("uid_table"));
-            /* Export to file (start a download) */
-            XLSX.writeFile(
-                wb,
-                "Membre-Groupe/" +
-                "Report-" +
-                now.getFullYear() +
-                "-" +
-                now.getMonth() +
-                "-" +
-                now.getDate() +
-                " at " +
-                now.getHours() +
-                "_" +
-                now.getMinutes() +
-                "_" +
-                now.getMilliseconds() +
-                ".xlsx"
-            );
-        }
-    });
-}
+// function download(profiles_hrefs, profiles_href_length) {
+//     var time = setInterval(() => {
+//         if (profiles_href_length == Object.keys(profiles_hrefs).length) {
+//             clearInterval(time);
+//             var allEntries = "";
+//             for (const i in profiles_hrefs) {
+//                 allEntries = allEntries.concat(i + " : " + profiles_hrefs[i] + "\n");
+//                 var no = document.createElement("td");
+//                 var no_text = document.createTextNode(i);
+//                 no.appendChild(no_text);
+//                 var uid = document.createElement("td");
+//                 var uid_text = document.createTextNode(profiles_hrefs[i]);
+//                 uid.appendChild(uid_text);
+//                 var tr = document.createElement("tr");
+//                 tr.appendChild(no);
+//                 tr.appendChild(uid);
+//                 var element = document.getElementById("list_of_uid");
+//                 element.appendChild(tr);
+//             }
+//             const now = new Date();
+//             loader.style.display = "none";
+//             const blob = new Blob([allEntries], {
+//                 type: "text/plain",
+//             });
+//             var url = URL.createObjectURL(blob);
+//             chrome.downloads
+//                 .download({
+//                     url: url,
+//                     filename:
+//                         "Membre-Groupe/" +
+//                         "Report-" +
+//                         now.getFullYear() +
+//                         "-" +
+//                         now.getMonth() +
+//                         "-" +
+//                         now.getDate() +
+//                         " at " +
+//                         now.getHours() +
+//                         "_" +
+//                         now.getMinutes() +
+//                         "_" +
+//                         now.getMilliseconds() +
+//                         ".txt",
+//                     conflictAction: "uniquify",
+//                 })
+//                 .then(onStartedDownload, onFailed);
+//             /* Create worksheet from HTML DOM TABLE */
+//             var wb = XLSX.utils.table_to_book(document.getElementById("uid_table"));
+//             /* Export to file (start a download) */
+//             XLSX.writeFile(
+//                 wb,
+//                 "Membre-Groupe/" +
+//                 "Report-" +
+//                 now.getFullYear() +
+//                 "-" +
+//                 now.getMonth() +
+//                 "-" +
+//                 now.getDate() +
+//                 " at " +
+//                 now.getHours() +
+//                 "_" +
+//                 now.getMinutes() +
+//                 "_" +
+//                 now.getMilliseconds() +
+//                 ".xlsx"
+//             );
+//         }
+//     });
+// }
